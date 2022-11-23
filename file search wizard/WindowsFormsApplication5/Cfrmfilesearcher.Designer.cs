b78291a84b295a0e65fa433f9db017d7e76c962c
Dipanshu <96021776+Dipanshu1210@users.noreply.github.com>
2022-11-23 19:13:36 +0530

    Add files via upload

---
+++ file search wizard/WindowsFormsApplication5/Cfrmfilesearcher.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Cfrmfilesearcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(25, 13);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(147, 13);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "Enter the File/Directory Name";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(28, 38);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(369, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(28, 64);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(369, 286);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // Cfrmfilesearcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 384);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblFileName);
+            this.Name = "Cfrmfilesearcher";
+            this.Text = "File Search ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ file search wizard/WindowsFormsApplication5/Cfrmfilesearcher.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Cfrmfilesearcher : Form
+    {
+        public Cfrmfilesearcher()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if(txtInput.Text !="")
+                {
+                    string fileName = txtInput.Text;
+
+                    if (File.Exists(fileName))
+                    {
+                        GetFileInformation(fileName);
+
+                        // we want ot read whats inside the file 
+                        StreamReader readar = null;
+                        try
+                        {
+                            using (readar = new StreamReader(fileName))
+                            {
+                                txtOutput.AppendText("File Contains : \n");
+
+                                txtOutput.AppendText(readar.ReadToEnd());
+
+                            }
+                        }
+                        catch (IOException)
+                        {
+                            MessageBox.Show("File Error", "File error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sorry, Your file does not exists!!!!!!!", "File not found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid string","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void GetFileInformation(string fileName)
+        {
+            txtOutput.Clear();
+            txtOutput.AppendText(fileName + "exists \n");
+
+            txtOutput.AppendText("Created : " + File.GetCreationTime(fileName) + "\n");
+
+            txtOutput.AppendText("Last accessed : " + File.GetLastAccessTime(fileName) + "\n");
+
+            txtOutput.AppendText("Last Modified : " + File.GetLastWriteTime(fileName) + "\n");
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
